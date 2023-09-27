06b376a6382bb5aa1a5854d92d26b2677147dcdb
wntjdals0214 <msgpakpak0214@naver.com>
2023-09-27 15:18:47 +0900

    Week4.2. 역피라미드 쌓기

---
+++ challenge/week4/pyramidReverse.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+	int floor;
+	printf("¸î ÃþÀ» ½×°Ú½À´Ï±î? (5~100)");
+	scanf_s("%d", &floor);
+
+	for (int i = floor; i > 0; i--) {
+		for (int j = 0; j < floor - i; j++) {
+		printf("s");
+	}
+		for (int k = 0; k < 2 * i - 1; k++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
